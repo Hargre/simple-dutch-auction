d010baa86ed6727e29a45923610066291c0ff6d1
Felipe Hargreaves <fhargreaves00@gmail.com>
2017-07-15 12:45:34 -0300

    Create main agents

---
+++ src/agents/AuctioneerAgent.java
@@ -0,0 +1,7 @@
+package agents;
+
+import jade.core.Agent;
+
+public class AuctioneerAgent extends Agent {
+	private static final long serialVersionUID = 667090917660533415L;
+}
---
+++ src/agents/BuyerAgent.java
@@ -0,0 +1,7 @@
+package agents;
+
+import jade.core.Agent;
+
+public class BuyerAgent extends Agent {
+	private static final long serialVersionUID = 76034717775332120L;
+}
