239f3bc1e67d7958bac0ea3a844befcc2e02ba23
Felipe Hargreaves <fhargreaves00@gmail.com>
2017-07-18 14:03:25 -0300

    Register Auctioneer Agent on DF service during setup.

--- src/agents/AuctioneerAgent.java
+++ src/agents/AuctioneerAgent.java
@@ -1,7 +1,27 @@
 package agents;
 
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 
 public class AuctioneerAgent extends Agent {
 	private static final long serialVersionUID = 667090917660533415L;
+	
+	protected void setup() {
+		DFAgentDescription auctioneerDescription = new DFAgentDescription();
+		auctioneerDescription.setName(getAID());
+		
+		ServiceDescription auctioneerService = new ServiceDescription();
+		auctioneerService.setName("auctioneer");
+		auctioneerService.setType("flower-auctioneer");
+		auctioneerDescription.addServices(auctioneerService);
+		
+		try {
+			DFService.register(this, auctioneerDescription);
+		} catch (FIPAException ex) {
+			ex.printStackTrace();
+		}
+	}
 }
