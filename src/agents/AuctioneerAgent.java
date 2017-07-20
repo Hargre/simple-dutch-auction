404e596b9967b2625faf99289cd9c7c0190f81da
Felipe Hargreaves <fhargreaves00@gmail.com>
2017-07-20 14:02:30 -0300

    Add search for buyers in auctioneer Agent.

--- src/agents/AuctioneerAgent.java
+++ src/agents/AuctioneerAgent.java
@@ -1,6 +1,12 @@
 package agents;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.FSMBehaviour;
+import jade.core.behaviours.OneShotBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
@@ -23,6 +29,8 @@ public class AuctioneerAgent extends Agent {
 		} catch (FIPAException ex) {
 			ex.printStackTrace();
 		}
+		
+		addBehaviour(new AuctioneerBehaviour());
 	}
 
 	@Override
@@ -35,5 +43,60 @@ public class AuctioneerAgent extends Agent {
 		}
 	}
 	
-	
+	/*
+	 * The agent will operate as a Finite State Machine (FSM).
+	 * Each state is represented by a internal Behaviour.
+	 */
+	private class AuctioneerBehaviour extends FSMBehaviour {
+		private static final long serialVersionUID = -7791885794766458598L;
+		private List<AID> buyers;
+		
+		public AuctioneerBehaviour() {
+			buyers = new ArrayList<>();
+			
+			registerFirstState(new SearchBuyersBehaviour(), "searching for buyers");
+			
+			/* Empty state only to simulate transition. 
+			 * Will be removed when proper end states are implemented.
+			 */
+			registerLastState(new OneShotBehaviour() {
+				private static final long serialVersionUID = 3438209180132381071L;
+
+				@Override
+				public void action() {
+				}
+			}, "ending auction");
+			
+			registerDefaultTransition("searching for buyers", "ending auction");
+		}
+		/*
+		 * Looks for existing buyers so the auction can start.
+		 */
+		private class SearchBuyersBehaviour extends OneShotBehaviour {
+			private static final long serialVersionUID = -5348961045040999273L;
+
+			@Override
+			public void action() {
+				DFAgentDescription template = new DFAgentDescription();
+				ServiceDescription serviceTemplate = new ServiceDescription();
+				serviceTemplate.setType("flower-buyer");
+				template.addServices(serviceTemplate);
+				
+				try {
+					DFAgentDescription[] result = DFService.search(myAgent, template);
+					if (result.length == 0) {
+						System.out.println("No buyers found...");
+					} else {
+						System.out.println("Buyers found:");
+						for (int i = 0; i < result.length; i++) {
+							buyers.add(result[i].getName());
+							System.out.println(buyers.get(i).getName());
+						}
+					}
+				} catch (FIPAException ex) {
+					ex.printStackTrace();
+				}
+			}		
+		}
+	}
 }
